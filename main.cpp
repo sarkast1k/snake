6ccad37910b46a0821cbd91aa317c33edc6fe426
sarkast1k <sarkast1k@pm.me>
2025-03-12 13:41:04 +0300

    added eating food and stopping the snake from reversing direction instantly

--- main.cpp
+++ main.cpp
@@ -1,96 +1,168 @@
-#include <iostream>
-#include <raylib.h>
-#include <deque>
-#include <raymath.h>
+  #include <iostream>
+  #include <raylib.h>
+  #include <deque>
+  #include <raymath.h>
 
-using namespace std;
+  using namespace std;
 
-Color Green = {173,204,96,255};
-Color DarkGreen = {43,51,24,255};
+  Color Green = {173,204,96,255};
+  Color DarkGreen = {43,51,24,255};
 
-int CellSize = 30;
-int CellCount = 25;
+  int CellSize = 30;
+  int CellCount = 25;
 
-double LastUpdateTime = 0;
+  double LastUpdateTime = 0;
 
-class Snake
-{
-public:
-    deque<Vector2> body = {Vector2{6,9}, Vector2{5,9}, Vector2{4,9}};
-    Vector2 direction = {1,0};
+  bool ElementInDeque(Vector2 element, deque<Vector2> deque)
+  {
+    for (unsigned int i = 0; i < deque.size(); i++)
+    {
+      if(Vector2Equals(deque[i], element))
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  bool EventTriggered(double interval)
+  {
+    double CurrentTime = GetTime();
+    if(CurrentTime - LastUpdateTime >= interval)
+    {
+      LastUpdateTime = CurrentTime;
+      return true;
+    }return false;
+  }
+
+  class Snake
+  {
+  public:
+      deque<Vector2> body = {Vector2{6,9}, Vector2{5,9}, Vector2{4,9}};
+      Vector2 direction = {1,0};
+
+      void Draw()
+    {
+      for (unsigned int i = 0; i < body.size(); i++)
+      {
+        float x = body[i].x;
+        float y = body[i].y;
+        Rectangle segment = Rectangle{x * CellSize, y* CellSize, (float)CellSize, (float)CellSize};
+        DrawRectangleRounded(segment, 0.5, 6, DarkGreen);
+      }
+    }
+
+    void Update()
+    {
+      body.pop_back();
+      body.push_front(Vector2Add(body[0], direction));
+    }
+      
+  };
+
+  class Food{
+  public:
+    Vector2 position;
+    Texture2D texture;
+
+    Food(deque<Vector2> snakeBody)
+    {
+      Image image = LoadImage("Textures/hood.png");
+      texture = LoadTextureFromImage(image);
+      UnloadImage(image);
+      position = GenerateRandomPos(snakeBody);
+    }
+
+    ~Food()
+    {
+      UnloadTexture(texture);
+    }
 
     void Draw()
-  {
-    for (unsigned int i = 0; i < body.size(); i++)
     {
-      float x = body[i].x;
-      float y = body[i].y;
-      Rectangle segment = Rectangle{x * CellSize, y* CellSize, (float)CellSize, (float)CellSize};
-      DrawRectangleRounded(segment, 0.5, 6, DarkGreen);
+      DrawTexture(texture, position.x * CellSize, position.y * CellSize, WHITE);
     }
-  }
 
-  void Update()
+    Vector2 GenerateRandomCell()
+    {
+      float x = GetRandomValue(0, CellCount - 1);
+      float y = GetRandomValue(0, CellCount - 1);
+      return Vector2{x,y};
+    }
+
+    Vector2 GenerateRandomPos(deque<Vector2> snakeBody)
+    {
+      Vector2 position = GenerateRandomCell();
+      while (ElementInDeque(position, snakeBody))
+      {
+        position = GenerateRandomCell();
+      }
+      return position;
+    }
+
+  };
+
+  class Game
   {
-    body.pop_back();
-    body.push_front(Vector2Add(body[0], direction));
-  }
-    
-};
+public: 
 
-class Food{
-public:
-   Vector2 position;
-   Texture2D texture;
+  Snake snake = Snake();
+  Food food = Food(snake.body);
 
-   Food()
-   {
-    Image image = LoadImage("Textures/hood.png");
-    texture = LoadTextureFromImage(image);
-    UnloadImage(image);
-    position = GenerateRandomPos();
-   }
-
-   ~Food()
-   {
-    UnloadTexture(texture);
-   }
-
-   void Draw()
-   {
-     DrawTexture(texture, position.x * CellSize, position.y * CellSize, WHITE);
-   }
-
-   Vector2 GenerateRandomPos()
-   {
-    float x = GetRandomValue(0, CellCount - 1);
-    float y = GetRandomValue(0, CellCount - 1);
-    return Vector2{x, y};
-  }
-
-};
-
-int main () 
-{
-
-  cout << "Starting the game..." << endl;
-
-  SetTargetFPS(60);
-  
-  InitWindow(CellSize * CellCount, CellSize * CellCount, "Snake Game");
-  Food food;
-  Snake snake;
-  while (WindowShouldClose() == 0)
+  void Draw()
   {
-    BeginDrawing();
-    snake.Update();
-    ClearBackground(Green);
     food.Draw();
     snake.Draw();
-    EndDrawing();
+  }
+  
+  void Update()
+  {
+    snake.Update();
+    EatFood();
   }
 
-  CloseWindow();
+  void EatFood()
+  {
+    if (Vector2Equals(snake.body[0], food.position))
+    {
+      food.position = food.GenerateRandomPos(snake.body);
+    }
+    
+  }
 
-return 0;
-}
+  };
+
+  int main () 
+  {
+
+    cout << "Starting the game..." << endl;
+
+    SetTargetFPS(60);
+    
+    InitWindow(CellSize * CellCount, CellSize * CellCount, "Snake Game");
+    
+    Game game = Game();
+
+    while (WindowShouldClose() == 0)
+    {
+      BeginDrawing();
+
+      if(EventTriggered(0.2))
+      {
+        game.Update();
+      } 
+
+      if (IsKeyPressed(KEY_UP) && game.snake.direction.y != 1){game.snake.direction = {0, -1};}
+      if (IsKeyPressed(KEY_DOWN) && game.snake.direction.y != -1){game.snake.direction = {0, 1};}
+      if (IsKeyPressed(KEY_LEFT) && game.snake.direction.x != 1){game.snake.direction = {-1, 0};}
+      if (IsKeyPressed(KEY_RIGHT) && game.snake.direction.x != -1){game.snake.direction = {1, 0};}
+
+      ClearBackground(DARKBLUE);
+      game.Draw();
+      EndDrawing();
+    }
+
+    CloseWindow();
+
+  return 0;
+  }
